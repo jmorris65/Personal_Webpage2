eec3ca75fc9bc9e8845fd01606537a64ac77feac
jmorris65 <jmorris65@users.noreply.github.com>
2020-03-17 01:32:23 -0400

    Design updates

--- public/style.js
+++ public/style.js
@@ -3,19 +3,11 @@ $("document").ready(function(){
 	$("button").click(function(event){
 		var selected = $(this).attr("target");
 
-		$(".display").animate({
-			opacity: 0,
-		}, 300, function() { 
-			$(this).css("display", "none");
-			$(this).toggleClass("display");
+		$(".display").css("display", "none");
+		$(".display").toggleClass("display");
 
-			$(selected).css("display", "block");
-			$(selected).animate({
-				opacity: 1,
-			}, 300, function(){
-				$(this).toggleClass("display")
-			});
-		});
+		$(selected).toggleClass("display");
+		$(selected).css("display", "block");
 
 		$(".selected").toggleClass("selected");
 		$(this).toggleClass("selected");
