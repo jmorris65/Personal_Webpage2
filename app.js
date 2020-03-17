ebf7ed8d17b4418c685362e67040b3ce6b2f9f15
jmorris65 <jmorris65@users.noreply.github.com>
2020-03-17 17:54:07 -0400

    Finishing designs and animations

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+
+// using express framework
 var express = require("express");
 var app = express();
 
+// setting public folder as custom asset
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+// root directory (only page)
+app.get("/*", function(req, res){
 	res.render("index.ejs");
 });
 
+// setting up local server
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log("App is running...");
--- public/style.js
+++ public/style.js
@@ -1,15 +1,32 @@
 $("document").ready(function(){
 
+	// setting up tabs
 	$("button").click(function(event){
 		var selected = $(this).attr("target");
 
-		$(".display").css("display", "none");
+		$(".display").css({
+			display: "none",
+			opacity: 0,
+		});
 		$(".display").toggleClass("display");
 
 		$(selected).toggleClass("display");
 		$(selected).css("display", "block");
 
+		$(selected).animate({opacity: 1}, "slow");
+
 		$(".selected").toggleClass("selected");
 		$(this).toggleClass("selected");
 	});
+
+	// setting up hover effect on last page
+	$(".last_page").hover(function() {
+		var selected = $(this).attr("target");
+
+		$(selected).animate({width: 0}, "fast");
+	}, function() {
+		var selected = $(this).attr("target");
+
+		$(selected).animate({width: "100%"}, "fast");
+	})
 });
